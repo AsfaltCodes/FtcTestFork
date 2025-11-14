63feb7d4eed4ae9276218d2f523b0be5d28ed148
Aysha <ayshatoma09@gmail.com>
2025-11-14 21:21:31 +0200

    DcMotor rightfront

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class TeleOp extends LinearOpMode {
+
+    DcMotor rightfront;
+
+    @Override
+
+public void runOpMode() throws InterruptedException{
+
+        rightfront= hardwareMap.dcMotor.get("idk");
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
