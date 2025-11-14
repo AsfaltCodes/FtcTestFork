14b754d8ef65747b4d2bfcba45ac0ce0b6f82871
Mihai <mihaimpmo@gmail.com>
2025-11-14 21:14:34 +0200

    declarare motoare

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motors.java
@@ -5,12 +5,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
-class MecanumTeleOp extends LinearOpMode {
+class Motors extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         DcMotor flMotor = hardwareMap.dcMotor.get("frontLeftMotor");
         DcMotor blMotor = hardwareMap.dcMotor.get("backLeftMotor");
         DcMotor frMotor = hardwareMap.dcMotor.get("frontRightMotor");
         DcMotor brMotor = hardwareMap.dcMotor.get("backRightMotor");
-    }
-}
+    }}
