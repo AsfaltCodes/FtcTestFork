0cc2ea850b9529fe165a1a861e53e4ef1d53fe00
lucam <lucamarkis05@gmail.com>
2025-11-14 21:11:43 +0200

    Am adaugat un motor principal corespunzator left stick-ului .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntroTeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntroTeleOP.java
@@ -3,20 +3,22 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-public class IntroTeleOP extends LinearOpMode {
+public class TeleOP extends LinearOpMode {
 
-    DcMotor dcMotor;
+    DcMotor prMotor;
 
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        dcMotor = hardwareMap.dcMotor.get("test");
-        waitForStart();
+        prMotor = hardwareMap.dcMotor.get("test");
         while (opModeIsActive()){
 
-            dcMotor.setPower(1);
+            float x;
 
+            x = gamepad1.left_stick_x;
+
+            prMotor.setPower(x);
 
         }
 
